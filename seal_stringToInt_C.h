fc4a5b6061fee8792c230f98ee22a81c4846a790
Mark Bishop <github.sealfin@sealfin.com>
2013-05-22 18:16:37 +0200

    Create seal_stringToInt_C.h

---
+++ seal_stringToInt_C.h
@@ -0,0 +1,28 @@
+#ifndef seal_stringToInt_C_h
+
+#define seal_stringToInt_C_h
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef enum
+{
+  k_seal_STRING_TO_INT_ERROR_C__NONE = 0,
+  k_seal_STRING_TO_INT_ERROR_C__P_IS_NULL,
+  k_seal_STRING_TO_INT_ERROR_C__P_IS_ZERO_CHARACTERS_LONG,
+  k_seal_STRING_TO_INT_ERROR_C__P_CONTAINS_ONLY_A_SIGN_CHARACTER,
+  k_seal_STRING_TO_INT_ERROR_C__SIGN_CHARACTER_IS_NOT_FIRST_CHARACTER,
+  k_seal_STRING_TO_INT_ERROR_C__P_CONTAINS_A_NON_NUMERICAL_CHARACTER
+}
+t_seal_STRING_TO_INT_ERROR_C;
+
+int f_seal_StringToInt_C( const char * const p );
+t_seal_STRING_TO_INT_ERROR_C f_seal_StringToInt_Error_C( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
