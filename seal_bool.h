27a2f8942c13658a112fc2f469a71e7a6ce0f2a5
Mark Bishop <github.sealfin@sealfin.com>
2013-05-28 21:23:54 +0200

    Create seal_bool.h

---
+++ seal_bool.h
@@ -0,0 +1,17 @@
+#ifndef seal_bool_h
+
+#define seal_bool_h
+
+#ifndef false
+#define false 0
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef __cplusplus
+typedef short bool;
+#endif
+
+#endif
