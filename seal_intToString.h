205d27d53c7c2074bfbf9a29e536b1aafc4c679f
Mark Bishop <github.sealfin@sealfin.com>
2013-05-28 21:25:16 +0200

    Create seal_intToString.h

---
+++ seal_intToString.h
@@ -0,0 +1,18 @@
+#ifndef seal_intToString_h
+
+#define seal_intToString_h
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "seal_bool.h"
+
+char *f_seal_IntToString( int p_int, const bool p_explicitSign );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
