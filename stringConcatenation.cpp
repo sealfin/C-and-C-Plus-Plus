2bc95325cd77c440ceba96b77a34e45212ce1af3
Mark Bishop <github.sealfin@sealfin.com>
2013-07-28 13:47:55 +0100

    Update stringConcatenation.cpp

--- stringConcatenation.cpp
+++ stringConcatenation.cpp
@@ -1,4 +1,5 @@
 #include "seal_stringConcatenation.h"
+#include "seal_stringConcatenation_C.h"
 #include "seal_stringConcatenation_shared.h"
 
-M_seal_CONCATENATE_STRINGS( f_seal_ConcatenateStrings, , throw "f_seal_ConcatenateStrings - p == NULL.";, throw "f_seal_ConcatenateStrings - the length of the concatenated string exceeds 2^(sizeof( size_t )*8)-2 characters."; )
+M_seal_CONCATENATE_STRINGS( f_seal_ConcatenateStrings, , throw "f_seal_ConcatenateStrings - p == NULL.";, throw "f_seal_ConcatenateStrings - the length of the concatenated string exceeds 2^(sizeof( size_t )*8)-2 characters.";, f_seal_StringConcatenation_PrintErrorsTo_C())
