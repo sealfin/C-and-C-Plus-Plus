22435aff7d8d90a78c17c4e8f2fbd3f29a2a2481
Mark Bishop <github.sealfin@sealfin.com>
2013-07-28 13:47:33 +0100

    Update stringConcatenation.c

--- stringConcatenation.c
+++ stringConcatenation.c
@@ -2,10 +2,21 @@
 #include "seal_stringConcatenation_shared.h"
 
 static t_seal_STRING_CONCATENATION_ERROR_C g_error = k_seal_STRING_CONCATENATION_ERROR_C__NONE;
+static FILE *g_printErrorsTo = NULL;
 
-M_seal_CONCATENATE_STRINGS( f_seal_ConcatenateStrings_C, g_error = k_seal_STRING_CONCATENATION_ERROR_C__NONE;, g_error = k_seal_STRING_CONCATENATION_ERROR_C__P_IS_NULL;, g_error = k_seal_STRING_CONCATENATION_ERROR_C__CONCATENATED_STRING_TOO_LONG; )
+M_seal_CONCATENATE_STRINGS( f_seal_ConcatenateStrings_C, g_error = k_seal_STRING_CONCATENATION_ERROR_C__NONE;, g_error = k_seal_STRING_CONCATENATION_ERROR_C__P_IS_NULL;, g_error = k_seal_STRING_CONCATENATION_ERROR_C__CONCATENATED_STRING_TOO_LONG;, g_printErrorsTo )
 
 t_seal_STRING_CONCATENATION_ERROR_C f_seal_StringConcatenation_Error_C( void )
 {
   return g_error;
+}
+
+void p_seal_StringConcatenation_PrintErrorsTo_C( FILE *p )
+{
+  g_printErrorsTo = p;
+}
+
+FILE *f_seal_StringConcatenation_PrintErrorsTo_C( void )
+{
+  return g_printErrorsTo;
 }
