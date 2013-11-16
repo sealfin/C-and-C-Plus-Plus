5420fac9d09819d32b853dd9b24b24e47822789f
Mark Bishop <github.sealfin@sealfin.com>
2013-11-16 14:37:34 +0000

    Create seal_sciEStringToDouble.h

---
+++ seal_sciEStringToDouble.h
@@ -0,0 +1,8 @@
+#ifndef seal_sciEStringToDouble_h
+
+#define seal_sciEStringToDouble_h
+
+double f_seal_SciEStringToDouble( const char * const p );
+/* Converts a string representation of a number in scientific e notation - eg. 0.0e0, equivalent to 0.0x10^0 - to a double. */
+
+#endif
