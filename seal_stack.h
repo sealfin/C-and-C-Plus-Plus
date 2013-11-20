efda27783d47af3c0f8844a13285ce6bb85ac919
Mark Bishop <github.sealfin@sealfin.com>
2013-11-20 18:30:34 +0000

    Create seal_stack.h

---
+++ seal_stack.h
@@ -0,0 +1,83 @@
+#ifndef seal_stack_h
+
+#define seal_stack_h
+
+#include <stdlib.h>
+
+template <class T>
+struct seal_StackNode
+{
+  struct seal_StackNode<T> *m_d;
+  T m_content;
+};
+
+template <class T>
+class seal_Stack
+{
+  private:
+    struct seal_StackNode<T> *m_top;
+
+  public:
+    seal_Stack( void )
+    {
+      m_top = NULL;
+    }
+
+    ~seal_Stack( void )
+    {
+      p_Delete( m_top );
+    }
+
+  private:
+    void p_Delete( struct seal_StackNode<T> *p )
+    {
+      if( p != NULL )
+      {
+        p_Delete( p->m_d );
+        p_Delete( p->m_content );
+        free( p );
+      }
+    }
+
+  public:
+    void p_Empty( void )
+    {
+      p_Delete( m_top );
+      m_top = NULL;
+    }
+
+    void p_Push( T p )
+    {
+      struct seal_StackNode<T> *n = ( struct seal_StackNode<T>* )malloc( sizeof( struct seal_StackNode<T> ));
+      n->m_d = m_top;
+      n->m_content = f_Assign( p );
+      m_top = n;
+    }
+
+    bool f_IsEmpty( void )
+    {
+      return ( m_top == NULL );
+    }
+
+    T f_Pop( void )
+    {
+      if( f_IsEmpty())
+        throw "seal_Stack::f_Pop - f_IsEmpty() == true.";
+      T content = m_top->m_content;
+      struct seal_StackNode<T> *n = m_top;
+      m_top = m_top->m_d;
+      free( n );
+      return content;
+    }
+
+    virtual T f_Assign( T p )
+    {
+      return p;
+    }
+
+    virtual void p_Delete( T p )
+    {
+    }
+};
+
+#endif
