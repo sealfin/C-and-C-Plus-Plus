c297050f30eb14c5fabbb6563216fba671cd3832
Mark Bishop <github.sealfin@sealfin.com>
2013-12-12 18:18:58 +0000

    Update stringComparison.c

--- stringComparison.c
+++ stringComparison.c
@@ -19,3 +19,28 @@ bool f_seal_CompareStrings_IgnoreCase( const char * const a, const char * const 
     return true;
   }
 }
+
+int f_seal_CompareStringsLexicographically_IgnoreCase( const char * const a, const char * const b )
+{
+  if(( a == NULL ) && ( b == NULL ))
+    return 0;
+  if(( a != NULL ) && ( b == NULL ))
+    return 1;
+  if(( a == NULL ) && ( b != NULL ))
+    return -1;
+  {
+    const size_t a_length = strlen( a ), b_length = strlen( b );
+    size_t i = 0;
+    for( ; ( i < a_length ) && ( i < b_length ); i ++ )
+    {
+      const char a_lowercase = tolower( a[ i ] ), b_lowercase = tolower( b[ i ] );
+      if( a_lowercase != b_lowercase )
+        return ( int )a_lowercase - ( int )b_lowercase;
+    }
+    if( a_length > b_length )
+      return 1;
+    if( b_length > a_length )
+      return -1;
+    return 0;
+  }
+}
