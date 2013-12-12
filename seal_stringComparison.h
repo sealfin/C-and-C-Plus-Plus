cb48917f637d49a6935c92cbd79138ba984e97e7
Mark Bishop <github.sealfin@sealfin.com>
2013-12-12 18:17:16 +0000

    Update seal_stringComparison.h

--- seal_stringComparison.h
+++ seal_stringComparison.h
@@ -14,6 +14,12 @@ bool f_seal_CompareStrings_IgnoreCase( const char * const a, const char * const 
 Returns true if a == NULL && b == NULL.
 Returns false if ( a == NULL || b == NULL ) && a != b.
 */
+int f_seal_CompareStringsLexicographically_IgnoreCase( const char * const a, const char * const b );
+/*
+Returns 0 if a == NULL && b == NULL.
+Returns 1 if a != NULL && b == NULL.
+Returns -1 if a == NULL && b != NULL.
+*/
 
 #ifdef __cplusplus
 }
