9d45bdbbe0dc8dc9d9394efaeb65742c50105f6d
Mark Bishop <github.sealfin@sealfin.com>
2015-08-08 17:10:05 +0100

    Update seal_stringQueue.h

--- seal_stringQueue.h
+++ seal_stringQueue.h
@@ -7,7 +7,7 @@
 
 class seal_StringQueue : public seal_Queue<char*>
 {
-  public:
+  private:
     char *f_Assign( char *p )
     {
       if( p == NULL )
@@ -20,6 +20,27 @@ class seal_StringQueue : public seal_Queue<char*>
     void p_Delete( char *p )
     {
       free( p );
+    }
+
+  public:
+    char *f_String( void )
+    {
+      struct seal_QueueNode<char*> *n = m_first;
+      size_t s_length = 0;
+      while( n != NULL )
+      {
+        s_length += strlen( n->m_content );
+        n = n->m_n;
+      }
+      char *s = ( char* )malloc( sizeof( char ) * ( s_length + 1 ));
+      s[ 0 ] = '\0';
+      n = m_first;
+      while( n != NULL )
+      {
+        strcat( s, n->m_content );
+        n = n->m_n;
+      }
+      return s;
     }
 };
 
